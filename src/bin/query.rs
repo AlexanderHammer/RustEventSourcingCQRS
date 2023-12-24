90ed19ec1e38b3c115078b8dc5266f36c377d0bd
Alexander Hammer <ahammer.dev@gmail.com>
2023-12-24 16:10:59 +0100

    inject mongodb client

--- src/bin/query.rs
+++ src/bin/query.rs
@@ -1,24 +1,19 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use mongodb::{
     bson::{doc, Document},
-    Client as MongoClient, Collection,
+    Client, Collection,
 };
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct Foo {
-    pub(crate) is_rust_a_nice_language: bool
+    pub(crate) is_rust_a_nice_language: bool,
 }
 
-
 #[get("/")]
-async fn get_movie() -> impl Responder {
-    // Replace the placeholder with your Atlas connection string
-    let uri = "mongodb://localhost:27017";
-    // Create a new client and connect to the server
-    let mongo_client = MongoClient::with_uri_str(uri).await.unwrap();
+async fn get_movie(client: web::Data<Client>) -> impl Responder {
     // Get a handle on the movies collection
-    let database = mongo_client.database("sample_mflix");
+    let database = client.database("sample_mflix");
     let my_coll: Collection<Document> = database.collection("movies");
     // Find a movie based on the title value
     let my_movie = my_coll
@@ -36,12 +31,16 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| "mongodb://localhost:27017".into());
+    let client = Client::with_uri_str(uri).await.expect("failed to connect");
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(client.clone()))
             .service(get_movie)
             .route("/hey", web::get().to(manual_hello))
     })
-        .bind(("localhost", 8080))?
-        .run()
-        .await
+    .bind(("localhost", 8080))?
+    .run()
+    .await
 }
