701544e4247dfd068b1d2ed255812d3698c886d8
Alexander Hammer <ahammer.dev@gmail.com>
2023-12-24 15:51:14 +0100

    accept http body on command side

--- src/bin/command.rs
+++ src/bin/command.rs
@@ -1,5 +1,6 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{error, post, web, App, HttpResponse, HttpServer, Responder};
 use eventstore::{Client as ESClient, EventData};
+use futures::StreamExt;
 use mongodb::{
     bson::{doc, Document},
     Client as MongoClient, Collection,
@@ -7,41 +8,54 @@ use mongodb::{
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub(crate) struct Foo {
-    pub(crate) is_rust_a_nice_language: bool
+pub(crate) struct LanguageVote {
+    pub(crate) lang: LanguageKind,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+enum LanguageKind {
+    JavaScript,
+    CSharp,
+    Rust,
+    CPlusPlus,
+    C,
+    Python,
+}
+
+const MAX_SIZE: usize = 262_144; // max payload size is 256k
+
 #[post("/language-vote")]
-async fn post_language_vote() -> impl Responder {
-    // Creates a client settings for a single node configuration.
+async fn post_language_vote(mut payload: web::Payload) -> impl Responder {
+    let mut body = web::BytesMut::new();
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk.unwrap();
+        // limit max size of in-memory payload
+        if (body.len() + chunk.len()) > MAX_SIZE {
+            return Err(error::ErrorBadRequest("overflow"));
+        }
+        body.extend_from_slice(&chunk);
+    }
+
+    let obj = serde_json::from_slice::<LanguageVote>(&body)?;
+    let evt = EventData::json("language-poll", &obj)?;
+
     let settings = "esdb://admin:changeit@localhost:2113?tls=false"
         .parse()
         .unwrap();
     let es_client = ESClient::new(settings).unwrap();
-
-    let payload = Foo {
-        is_rust_a_nice_language: true,
-    };
-
-    // It is not mandatory to use JSON as a data format however EventStoreDB
-    // provides great additional value if you do so.
-    let evt = EventData::json("language-poll", &payload).unwrap();
 
     es_client
         .append_to_stream("language-stream", &Default::default(), evt)
         .await
         .unwrap();
 
-    HttpResponse::Ok().body("Hey there!")
+    Ok(HttpResponse::Ok().body(serde_json::to_string_pretty(&obj).unwrap()))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(post_language_vote)
-    })
-    .bind(("localhost", 8081))?
-    .run()
-    .await
+    HttpServer::new(move || App::new().service(post_language_vote))
+        .bind(("localhost", 8081))?
+        .run()
+        .await
 }
--- src/bin/query.rs
+++ src/bin/query.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use mongodb::{
     bson::{doc, Document},
     Client as MongoClient, Collection,
