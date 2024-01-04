6161efa234052fa8ef14c047e1494eaac52b3e10
Alexander Hammer <50288713+AlexanderHammer@users.noreply.github.com>
2024-01-04 19:43:01 +0100

    Update command.rs
    added endpoint to create items

--- src/bin/command.rs
+++ src/bin/command.rs
@@ -1,28 +1,22 @@
 use actix_web::{error, post, web, App, HttpResponse, HttpServer, Responder};
-use eventstore::{Client as ESClient, EventData};
+use eventstore::{Client, EventData, AppendToStreamOptions};
 use futures::StreamExt;
-use mongodb::{bson::{doc, Document}, Client as MongoClient, Client, Collection};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub(crate) struct LanguageVote {
-    pub(crate) lang: LanguageKind,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-enum LanguageKind {
-    JavaScript,
-    CSharp,
-    Rust,
-    CPlusPlus,
-    C,
-    Python,
+pub(crate) struct CreateStockItem {
+    pub(crate) part_no: String,
+    pub(crate) name: String,
+    pub(crate) category: String,
+    pub(crate) count: u64
 }
 
 const MAX_SIZE: usize = 262_144; // max payload size is 256k
 
-#[post("/language-vote")]
-async fn post_language_vote(es_client: web::Data<ESClient>,  mut payload: web::Payload) -> impl Responder {
+#[post("/stock-item")]
+async fn post_stock_item(es_client: web::Data<Client>,  mut payload: web::Payload) -> impl Responder {
+
     let mut body = web::BytesMut::new();
     while let Some(chunk) = payload.next().await {
         let chunk = chunk?;
@@ -33,15 +27,18 @@ async fn post_language_vote(es_client: web::Data<ESClient>,  mut payload: web::P
         body.extend_from_slice(&chunk);
     }
 
-    let obj = serde_json::from_slice::<LanguageVote>(&body)?;
-    let evt = EventData::json("language-poll", &obj)?;
+    let obj = serde_json::from_slice::<CreateStockItem>(&body)?;
+    let evt = EventData::json("stock-item-created", &obj)?.id(Uuid::new_v4());
+
+    let options = AppendToStreamOptions::default().expected_revision(eventstore::ExpectedRevision::NoStream);
 
     es_client
-        .append_to_stream("language-stream", &Default::default(), evt)
+        .append_to_stream(obj.part_no, &options, evt)
         .await
         .unwrap();
 
-    Ok(HttpResponse::Ok().body(serde_json::to_string_pretty(&obj)?))
+
+    Ok(HttpResponse::Ok())
 }
 
 #[actix_web::main]
@@ -49,11 +46,11 @@ async fn main() -> std::io::Result<()> {
     let settings = "esdb://admin:changeit@localhost:2113?tls=false"
         .parse()
         .unwrap();
-    let es_client = ESClient::new(settings).unwrap();
+    let es_client = Client::new(settings).unwrap();
 
     HttpServer::new(move || {
         App::new()
-            .service(post_language_vote)
+            .service(post_stock_item)
             .app_data(web::Data::new(es_client.clone()))
     })
     .bind(("localhost", 8081))?
