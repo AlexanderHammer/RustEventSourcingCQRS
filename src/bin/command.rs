b17726b8ca8b99f80d3d78afd09dc2f1bb506379
Alexander Hammer <ahammer.dev@gmail.com>
2024-01-07 18:29:37 +0100

    accept generic events also

--- src/bin/command.rs
+++ src/bin/command.rs
@@ -1,8 +1,8 @@
-use std::error::Error;
 use actix_web::{error, post, web, App, HttpResponse, HttpServer, Responder};
 use eventstore::{AppendToStreamOptions, Client, EventData};
 use futures::StreamExt;
 use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, error::Error};
 use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -14,30 +14,67 @@ pub(crate) struct CreateStockItem {
     pub(crate) count: u64,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct CreateGenericEvent {
+    pub(crate) stream_name: String,
+    pub(crate) prefix: String,
+    pub(crate) event_type: String,
+    pub(crate) data: HashMap<String, String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct GenericEvent {
+    pub(crate) data: HashMap<String, String>,
+}
+
 const MAX_SIZE: usize = 262_144; // max payload size is 256k
 
 #[post("/stock-item")]
-async fn post_stock_item(
-    es_client: web::Data<Client>,
-    payload: web::Payload,
-) -> impl Responder {
+async fn post_stock_item(es_client: web::Data<Client>, payload: web::Payload) -> impl Responder {
     let body = payload_to_bytes_mut(payload).await?;
-    let obj = serde_json::from_slice::<CreateStockItem>(&body)?;
-    let evt = EventData::json("create-stock-item", &obj)?.id(Uuid::new_v4());
+    let command = serde_json::from_slice::<CreateStockItem>(&body)?;
+    let evt = EventData::json("create-stock-item", &command)?.id(Uuid::new_v4());
 
     let options =
         AppendToStreamOptions::default().expected_revision(eventstore::ExpectedRevision::NoStream);
 
-    let stream_name = format!("stockItem-{}", obj.part_no);
+    let stream_name = format!("stockItem-{}", command.part_no);
     let append_result = es_client.append_to_stream(stream_name, &options, evt);
 
     match append_result.await {
         Ok(_) => return Ok(HttpResponse::Accepted()),
-        Err(_) => Err(error::ErrorExpectationFailed("Stock item already exists"))
+        Err(_) => Err(error::ErrorExpectationFailed("Stock item already exists")),
     }
 }
 
-async fn payload_to_bytes_mut(mut payload: web::Payload) -> Result<web::BytesMut, Box<dyn Error>>   {
+#[post("/generic-event")]
+async fn post_generic_event(es_client: web::Data<Client>, payload: web::Payload) -> impl Responder {
+    let body = payload_to_bytes_mut(payload).await?;
+    let command = serde_json::from_slice::<CreateGenericEvent>(&body)?;
+    if command.prefix.is_empty() || command.stream_name.is_empty() || command.event_type.is_empty()
+    {
+        return Err(error::ErrorBadRequest(format!(
+            "Prefix, stream name and event type are required: {}, {}, {}",
+            command.prefix, command.stream_name, command.event_type
+        )));
+    }
+    let evt = EventData::json(&command.event_type, GenericEvent { data: command.data })?
+        .id(Uuid::new_v4());
+    let options = AppendToStreamOptions::default();
+    let stream_name = format!("{}-{}", command.prefix, command.stream_name);
+
+    let append_result = es_client.append_to_stream(stream_name, &options, evt);
+
+    match append_result.await {
+        Ok(_) => return Ok(HttpResponse::Accepted()),
+        Err(e) => Err(error::ErrorImATeapot(format!(
+            "Error occurred while appending event: {}",
+            e.to_string()
+        ))),
+    }
+}
+
+async fn payload_to_bytes_mut(mut payload: web::Payload) -> Result<web::BytesMut, Box<dyn Error>> {
     let mut body = web::BytesMut::new();
     while let Some(chunk) = payload.next().await {
         let chunk = chunk?;
@@ -60,6 +97,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .service(post_stock_item)
+            .service(post_generic_event)
             .app_data(web::Data::new(es_client.clone()))
     })
     .bind(("localhost", 8081))?
--- src/bin/command_handler.rs
+++ src/bin/command_handler.rs
@@ -7,13 +7,15 @@ async fn main(){
     .parse()
     .unwrap();
     let es_client = Client::new(settings).unwrap();
-    read_all_events(es_client).await;
+    if let Err(err) = read_all_events(es_client).await {
+        eprintln!("Error while reading events: {}", err);
+    }
 }   
 
 
 async fn read_all_events(client: Client) -> Result<(), Box<dyn Error>>  {
     let retry = RetryOptions::default().retry_forever();
-    let filter = SubscriptionFilter::on_event_type().exclude_system_events();
+    let filter = SubscriptionFilter::on_event_type().add_prefix("stockItem").exclude_system_events();
     let options = SubscribeToAllOptions::default().filter(filter).retry_options(retry);
     let mut sub = client.subscribe_to_all(&options).await;
 
@@ -22,6 +24,6 @@ async fn read_all_events(client: Client) -> Result<(), Box<dyn Error>>  {
         let stream_id = event.get_original_stream_id();
         let revision = event.get_original_event().revision;
 
-        println!("Received event {}@{}", revision, stream_id);
+        println!("Received event {}@{}, {:?}", revision, stream_id, event.get_original_event());
     }
 }
