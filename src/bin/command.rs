fe9ee45827583bed574a9ca70c3d54591a920f0c
Alexander Hammer <ahammer.dev@gmail.com>
2024-01-10 13:02:19 +0100

    added set/add/remove/delete

--- src/bin/command.rs
+++ src/bin/command.rs
@@ -1,4 +1,4 @@
-use actix_web::{error, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{delete, error, post, put, web, App, HttpResponse, HttpServer, Responder};
 use eventstore::{AppendToStreamOptions, Client, EventData};
 use futures::StreamExt;
 use serde::{Deserialize, Serialize};
@@ -12,6 +12,17 @@ pub(crate) struct CreateStockItem {
     pub(crate) description: String,
     pub(crate) category: String,
     pub(crate) count: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct AdjustStockItem {
+    pub(crate) part_no: String,
+    pub(crate) count: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct DeleteStockItem {
+    pub(crate) part_no: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -47,11 +58,97 @@ async fn post_stock_item(es_client: web::Data<Client>, payload: web::Payload) ->
     }
 }
 
+#[post("/stock-item/add/{part_no}/{count}")]
+async fn add_amount(
+    es_client: web::Data<Client>,
+    path: web::Path<(String, u64)>,
+) -> impl Responder {
+    let (part_no, count) = path.into_inner();
+    let command = AdjustStockItem { part_no, count };
+    let evt = EventData::json("add-stock-item", &command)?.id(Uuid::new_v4());
+
+    let options = AppendToStreamOptions::default()
+        .expected_revision(eventstore::ExpectedRevision::StreamExists);
+
+    let stream_name = format!("stockItem-{}", command.part_no);
+    let append_result = es_client.append_to_stream(stream_name, &options, evt);
+
+    match append_result.await {
+        Ok(_) => return Ok(HttpResponse::Accepted()),
+        Err(_) => Err(error::ErrorExpectationFailed("Who knows")),
+    }
+}
+
+#[put("/stock-item/set/{part_no}/{count}")]
+async fn set_amount(
+    es_client: web::Data<Client>,
+    path: web::Path<(String, u64)>,
+) -> impl Responder {
+    let (part_no, count) = path.into_inner();
+    let command = AdjustStockItem { part_no, count };
+    let evt = EventData::json("set-stock-item", &command)?.id(Uuid::new_v4());
+
+    let options = AppendToStreamOptions::default()
+        .expected_revision(eventstore::ExpectedRevision::StreamExists);
+
+    let stream_name = format!("stockItem-{}", command.part_no);
+    let append_result = es_client.append_to_stream(stream_name, &options, evt);
+
+    match append_result.await {
+        Ok(_) => return Ok(HttpResponse::Accepted()),
+        Err(_) => Err(error::ErrorExpectationFailed("Who knows")),
+    }
+}
+
+#[delete("/stock-item/{part_no}")]
+async fn delete_stock_item(
+    es_client: web::Data<Client>,
+    path: web::Path<String>,
+) -> impl Responder {
+    let part_no = path.into_inner();
+    let command = DeleteStockItem { part_no };
+    let evt = EventData::json("delete-stock-item", &command)?.id(Uuid::new_v4());
+
+    let options = AppendToStreamOptions::default()
+        .expected_revision(eventstore::ExpectedRevision::StreamExists);
+
+    let stream_name = format!("stockItem-{}", command.part_no);
+    let append_result = es_client.append_to_stream(stream_name, &options, evt);
+
+    match append_result.await {
+        Ok(_) => return Ok(HttpResponse::Accepted()),
+        Err(_) => Err(error::ErrorExpectationFailed("Who knows")),
+    }
+}
+
+#[post("/stock-item/remove/{part_no}/{count}")]
+async fn remove_amount(
+    es_client: web::Data<Client>,
+    path: web::Path<(String, u64)>,
+) -> impl Responder {
+    let (part_no, count) = path.into_inner();
+    let command = AdjustStockItem { part_no, count };
+    let evt = EventData::json("remove-stock-item", &command)?.id(Uuid::new_v4());
+
+    let options = AppendToStreamOptions::default()
+        .expected_revision(eventstore::ExpectedRevision::StreamExists);
+
+    let stream_name = format!("stockItem-{}", command.part_no);
+    let append_result = es_client.append_to_stream(stream_name, &options, evt);
+
+    match append_result.await {
+        Ok(_) => return Ok(HttpResponse::Accepted()),
+        Err(_) => Err(error::ErrorExpectationFailed("Who knows")),
+    }
+}
+
 #[post("/generic-event")]
 async fn post_generic_event(es_client: web::Data<Client>, payload: web::Payload) -> impl Responder {
     let body = payload_to_bytes_mut(payload).await?;
     let command = serde_json::from_slice::<CreateGenericEvent>(&body)?;
-    if command.stream_prefix.is_empty() || command.stream_name.is_empty() || command.event_type.is_empty()
+    if command.stream_prefix.is_empty()
+        || command.stream_name.is_empty()
+        || command.event_type.is_empty()
     {
         return Err(error::ErrorBadRequest(format!(
             "Stream prefix, stream name and event type are required: {}, {}, {}",
@@ -98,6 +195,10 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(post_stock_item)
             .service(post_generic_event)
+            .service(add_amount)
+            .service(remove_amount)
+            .service(delete_stock_item)
+            .service(set_amount)
             .app_data(web::Data::new(es_client.clone()))
     })
     .bind(("localhost", 8081))?
